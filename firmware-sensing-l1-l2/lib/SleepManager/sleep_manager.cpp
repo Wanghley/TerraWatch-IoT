8ff5818e1a8427b1c2a13fdfae6273e5b95c76f1
wanghley <me@wanghley.com>
2025-11-16 11:52:58 -0500

    feat: enhance SleepManager with debug mode and detailed wake-up logging

--- firmware-sensing-l1-l2/lib/SleepManager/sleep_manager.cpp
+++ firmware-sensing-l1-l2/lib/SleepManager/sleep_manager.cpp
@@ -1,24 +1,62 @@
 #include "sleep_manager.h"
 #include <Arduino.h>
 
-SleepManager::SleepManager(int lpir, int cpir, int rpir)
-  : _lpir(lpir), _cpir(cpir), _rpir(rpir) {
-  // Calculate the bitmask for the pins
-  _wakeMask = (1ULL << _lpir) | (1ULL << _cpir) | (1ULL << _rpir);
-}
+SleepManager::SleepManager(int lpir, int cpir, int rpir, bool debug)
+    : _lpir(lpir), _cpir(cpir), _rpir(rpir), _wakeMask(0), _debug(debug) {}
 
 void SleepManager::configure() {
-  // Set pins as inputs
-  pinMode(_lpir, INPUT);
-  pinMode(_cpir, INPUT);
-  pinMode(_rpir, INPUT);
+    pinMode(_lpir, INPUT);
+    pinMode(_cpir, INPUT);
+    pinMode(_rpir, INPUT);
+    if (_debug) {
+        Serial.begin(115200);
+        delay(200);
+        Serial.println("SleepManager: configure()");
+    }
 
-  // Configure the wake-up source
-  // We will wake up if ANY of the pins in the mask go HIGH
-  esp_sleep_enable_ext1_wakeup(_wakeMask, ESP_EXT1_WAKEUP_ANY_HIGH);
+    esp_sleep_wakeup_cause_t cause = esp_sleep_get_wakeup_cause();
+    if (_debug) {
+        Serial.printf("wake cause code: %d\n", cause);
+    }
+
+    switch (cause) {
+        case ESP_SLEEP_WAKEUP_EXT1: {
+            // small settle time and read EXT1 mask
+            delay(50);
+            uint64_t mask = esp_sleep_get_ext1_wakeup_status();
+            _wakeMask = mask;
+            bool l = (mask & (1ULL << _lpir)) != 0;
+            bool c = (mask & (1ULL << _cpir)) != 0;
+            bool r = (mask & (1ULL << _rpir)) != 0;
+            if (_debug) {
+                Serial.printf("EXT1 mask bits: L=%d C=%d R=%d\n", l, c, r);
+            }
+            break;
+        }
+        case ESP_SLEEP_WAKEUP_TIMER:
+            if (_debug) Serial.println("Wake reason: TIMER");
+            break;
+        case ESP_SLEEP_WAKEUP_TOUCHPAD:
+            if (_debug) Serial.println("Wake reason: TOUCHPAD");
+            break;
+        case ESP_SLEEP_WAKEUP_EXT0:
+            if (_debug) Serial.println("Wake reason: EXT0");
+            break;
+        case ESP_SLEEP_WAKEUP_UNDEFINED:
+        default:
+            if (_debug) Serial.println("Wake reason: UNDEFINED/OTHER");
+            break;
+    }
 }
 
 void SleepManager::goToSleep() {
-  Serial.flush(); // Finish printing before sleeping
-  esp_light_sleep_start();
+    if (_debug) {
+        Serial.println("SleepManager: going to light sleep (EXT1 enabled)...");
+        Serial.flush();
+    }
+    uint64_t wakeMask = (1ULL << _lpir) | (1ULL << _cpir) | (1ULL << _rpir);
+    esp_sleep_enable_ext1_wakeup(wakeMask, ESP_EXT1_WAKEUP_ANY_HIGH);
+    delay(100); // allow pins to settle
+    // light sleep
+    esp_light_sleep_start();
 }
--- firmware-sensing-l1-l2/lib/SleepManager/sleep_manager.h
+++ firmware-sensing-l1-l2/lib/SleepManager/sleep_manager.h
@@ -1,19 +1,15 @@
 #pragma once // Prevents file from being included multiple times
-
 #include "esp_sleep.h"
-
 class SleepManager {
 public:
     // Constructor: Takes the PIR sensor pins
-    SleepManager(int lpir, int cpir, int rpir);
-
+    SleepManager(int lpir, int cpir, int rpir, bool debug = false);
     // Sets up the GPIOs and wake-up sources
     void configure();
-
     // Puts the ESP32 into light sleep
     void goToSleep();
-
 private:
     int _lpir, _cpir, _rpir;
     uint64_t _wakeMask;
+    bool _debug;
 };
