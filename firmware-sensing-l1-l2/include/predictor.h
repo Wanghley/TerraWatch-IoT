f29c1984c39c5dcefeacb6c4f4d4bf94966fcc43
Wanghley Soares Martins <wanghleys@gmail.com>
2025-11-20 21:48:51 -0500

    feat: implement Predictor class for DNN model inference with TensorFlow Lite

---
+++ firmware-sensing-l1-l2/include/predictor.h
@@ -0,0 +1,43 @@
+#ifndef PREDICTOR_H
+#define PREDICTOR_H
+
+#include <Arduino.h>
+#include "shared_types.h" // Access to SensorPacket
+#include <TensorFlowLite_ESP32.h>
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+
+// Include your model data
+#include "model.h" 
+
+class Predictor {
+public:
+    bool begin();
+    // Returns -1.0 if buffering, or 0.0-1.0 if prediction made
+    float update(const SensorPacket& pkt); 
+
+private:
+    // TFLite boilerplate
+    const tflite::Model* model = nullptr;
+    tflite::MicroInterpreter* interpreter = nullptr;
+    TfLiteTensor* input = nullptr;
+    TfLiteTensor* output = nullptr;
+    
+    // 40KB Arena should be enough
+    static const int kTensorArenaSize = 40 * 1024; 
+    uint8_t tensor_arena[kTensorArenaSize];
+
+    // Buffering for Time Series
+    // 198 steps x 12 features (Must match Python training)
+    static const int SEQ_LEN = 198; 
+    static const int NUM_FEATS = 12;
+    
+    int buffer_index = 0;
+
+    // Helper functions
+    float get_max(const float* arr, int len);
+    float get_mean(const float* arr, int len);
+};
+
+#endif
